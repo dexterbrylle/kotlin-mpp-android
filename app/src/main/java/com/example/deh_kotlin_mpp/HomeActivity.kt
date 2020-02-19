d5f934d59a445bcf25ccf48feeef7861ae175031
Matos, Dexter Brylle <dexterbrylle.matos@finastra.com>
2020-02-20 04:24:26 +0800

    Added TextView to HomeAct

--- app/src/main/java/com/example/deh_kotlin_mpp/HomeActivity.kt
+++ app/src/main/java/com/example/deh_kotlin_mpp/HomeActivity.kt
@@ -1,9 +1,14 @@
 package com.example.deh_kotlin_mpp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
+import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class HomeActivity : AppCompatActivity() {
@@ -12,5 +17,23 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
 
         findViewById<TextView>(R.id.main_text).text = createApplicationScreenMessage()
+
+        val db = FirebaseFirestore.getInstance()
+        val auth = FirebaseAuth.getInstance()
+        var currentUser = null
+
+        db.collection("users")
+            .whereEqualTo("uid", auth.currentUser?.uid)
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d("Home", "${document.id} => ${document.data}")
+                    findViewById<TextView>(R.id.main_text_2).text = "Hello there, ${document.getString("first_name")} ${document.getString("last_name")}"
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("Home", "Error getting documents.", exception)
+            }
+
     }
 }
